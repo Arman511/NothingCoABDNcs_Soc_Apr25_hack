06dcba024d4080d7f05b16bef1098f6227061704
Yousef Abdalla <u03ya23@abdn.ac.uk>
2025-04-19 14:07:04 +0100

    Refactor student model and routes for better code organization

--- students/models.py
+++ students/models.py
@@ -11,12 +11,12 @@ class Student:
 
         from app import db
 
-        if db.students_collection.find_one({"username": student["email"]}):
+        if db.students.find_one({"username": student["email"]}):
             return jsonify({"error": "Student already exists"}), 400
 
         # Hash the password and save the student
         hashed_password = pbkdf2_sha512.hash(student["password"])
-        db.students_collection.insert_one(
+        db.students.insert_one(
             {
                 "_id": uuid.uuid1().hex,
                 "email": student["email"],
@@ -25,3 +25,59 @@ class Student:
         )
 
         return jsonify({"message": "Student added successfully"}), 201
+
+    def get_student_by_email(self, student):
+
+        from app import db
+
+        student = db.students.find_one({"email": student["email"]})
+        if student is None:
+            return jsonify({"error": "Student not found"}), 404
+
+        return jsonify({"message": "Student found successfully"}), 200
+
+    def get_students(self):
+        """Get all students from the database"""
+
+        from app import db
+
+        students = db.students.find()
+        if not students:
+            return jsonify({"error": "No students found"}), 404
+
+        return jsonify({"students": list(students)}), 200
+
+    def delete_student(self, student):
+        """Delete a student from the database"""
+
+        from app import db
+
+        if not db.students.find_one({"email": student["email"]}):
+            return jsonify({"error": "Student not found"}), 404
+
+        db.students.delete_one({"email": student["email"]})
+
+        return jsonify({"message": "Student deleted successfully"}), 200
+
+    def update_student_course_info(self, student):
+        """Update a student in the database"""
+
+        from app import db
+
+        if not db.students.find_one({"email": student["email"]}):
+            return jsonify({"error": "Student not found"}), 404
+
+        db.students_collection.update_one(
+            {"email": student["email"]},
+            {
+                "$set": {
+                    "course": student["course"],
+                }
+            },
+        )
+
+        return jsonify({"message": "Student updated successfully"}), 200
+    
+    
+    
+    
