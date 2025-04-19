8ac222deda6a9f9f45124a9d22f8deee17b98f75
Arman Shaikh <arman.shaikh.work@gmail.com>
2025-04-19 14:44:54 +0100

    Add course update functionality for students; implement frontend form and backend route

--- students/routes_students.py
+++ students/routes_students.py
@@ -1,4 +1,7 @@
 from flask import jsonify, redirect, render_template, request, session
+
+from course import course
+import route_wrapper
 from .models import Student
 
 
@@ -50,3 +53,28 @@ def add_student_routes(app):
             return redirect("/student/login")
 
         return render_template("dash_stu.html")
+
+    @app.route("/student/add_update_courses", methods=["GET", "POST"])
+    @route_wrapper.student_login_required
+    def student_add_update_courses():
+        """
+        Route for adding or updating courses.
+        """
+        if "student" not in session:
+            return redirect("/student/login")
+
+        if request.method == "POST":
+            data = request.get_json()
+            if not data:
+                return jsonify({"error": "No data provided"}), 400
+
+            courses = data.get("courses")
+
+            if not courses:
+                return jsonify({"error": "Courses are required"}), 400
+
+            return Student().update_student_course_info(data)
+
+        return render_template(
+            "add_update_courses.html", courses=course.get_all_courses()
+        )
